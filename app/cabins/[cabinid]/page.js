6352d249dbd382cf89e408d43a4c18fee34fd379
Kacper <rohhan.514@gmail.com>
2024-07-16 20:24:26 +0200

    init commit

---
+++ app/cabins/[cabinid]/page.js
@@ -0,0 +1,43 @@
+import Reservation from "@/app/_components/Reservation";
+import Spinner from "@/app/_components/Spinner";
+import { getCabin, getCabins } from "@/app/_lib/data-service";
+import { Suspense } from "react";
+import Cabin from "@/app/_components/Cabin";
+export async function generateMetadata({ params }) {
+  const { name, description, image } = await getCabin(params.cabinid);
+
+  return {
+    title: `Cabin ${name}`,
+    description: description,
+    image: image,
+  };
+}
+
+export async function generateStaticParams() {
+  const cabins = await getCabins();
+
+  const ids = cabins.map((cabin) => ({
+    cabinid: String(cabin.id),
+  }));
+
+  console.log(ids);
+  return ids;
+}
+
+export default async function Page({ params }) {
+  const cabin = await getCabin(params.cabinid);
+
+  return (
+    <div className="max-w-6xl mx-auto mt-8">
+      <Cabin cabin={cabin} />
+      <div>
+        <h2 className="mb-10 text-5xl font-semibold text-center text-accent-400">
+          Reserve {cabin.name} today. Pay on arrival.
+        </h2>
+        <Suspense fallback={<Spinner />}>
+          <Reservation cabin={cabin} />
+        </Suspense>
+      </div>
+    </div>
+  );
+}
